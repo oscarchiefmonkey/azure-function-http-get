4c530018efdbc92e3dafe07a618e47e480c04459
Chief Monkey <oscar.thorin@chiefmonkey.se>
2025-01-30 11:05:31 +0100

    add many code

--- MyFunction.cs
+++ MyFunction.cs
@@ -18,6 +18,13 @@ namespace FunctionApp1
         public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
+            _logger.LogTrace("This is a TRACE log message");
+            _logger.LogDebug("This is a DEBUG log message");
+            _logger.LogInformation("This is a INFORMATION log message");
+            _logger.LogWarning("This is a WARNING log message");
+            _logger.LogError("This is a ERROR log message");
+            _logger.LogCritical("This is a CRITICAL log message");
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
             return new OkObjectResult("Welcome to Azure Functions!");
         }
     }
--- Program.cs
+++ Program.cs
@@ -1,7 +1,34 @@
-using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Hosting;
-var builder = FunctionsApplication.CreateBuilder(args);
-builder.ConfigureFunctionsWebApplication();
-// Application Insights isn't enabled by default. See https://aka.ms/AAt8mw4.
-builder.Services.AddApplicationInsightsTelemetryWorkerService().ConfigureFunctionsApplicationInsights();
-builder.Build().Run();
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+
+var host = new HostBuilder()
+    .ConfigureFunctionsWebApplication()
+    .ConfigureServices(services =>
+    {
+        services.AddApplicationInsightsTelemetryWorkerService();
+        services.ConfigureFunctionsApplicationInsights();
+        services.Configure<LoggerFilterOptions>(options =>
+        {
+            LoggerFilterRule toRemove = options.Rules.FirstOrDefault(rule => rule.ProviderName
+                == "Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider");
+
+            if (toRemove is not null)
+            {
+                options.Rules.Remove(toRemove);
+            }
+        });
+    })
+    .ConfigureLogging((hostingContext, logging) =>
+    {
+        logging.AddApplicationInsights(console =>
+        {
+            console.IncludeScopes = true;
+        });
+
+        logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+    })
+    .Build();
+
+host.Run();
